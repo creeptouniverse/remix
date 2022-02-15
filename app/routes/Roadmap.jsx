714723ee83fd5030279beec6a266fb99eebac551
creeptouniverse <creeptouniverse@gmail.com>
2022-02-15 17:46:30 +0545

    add

--- app/routes/Roadmap.jsx
+++ app/routes/Roadmap.jsx
@@ -2,6 +2,9 @@ import { Link } from "remix";
 export default function Roadmap(){
     return(
         <section>
+          <div className="upnav">
+            <Link to="/">Home</Link>
+            </div>
             <div>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
