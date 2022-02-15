1cd8fa88de13efebf13a25aa89bca0c427b2730b
creeptouniverse <creeptouniverse@gmail.com>
2022-02-15 12:55:01 +0545

    roadmap

--- app/routes/Roadmap.jsx
+++ app/routes/Roadmap.jsx
@@ -1,208 +1,95 @@
 import { Link } from "remix"
 export default function Roadmap(){
     return(
-        <section className="roadmap" id="roadmap">
-            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'/>
-<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'/>
-<div className="bg">
-  <div className="text">
-  <h1>Journey to The Multiverse</h1>
-  <p>Minting begins. Space Explorars are freed on the Ethereum, Polygon Blockchain.</p></div>
-  <div className="container">
-    <div className="row py-5">
-      <div className="col-md-12">
-        <div className="main-timeline">
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>1.</h4>
+        <section>
+            <div>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <div className="bg">
+          <h1>CreepTo Roadmap</h1>
+          <p>Roadmap of what we are doing and our future goals.</p>
+          <div className="container">
+            <div className="row py-5">
+              <div className="col-md-12">
+                <div className="main-timeline">
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Present</h4>
+                      </div>
+                      <h4 className="title">At present we are at the initial phase of our marketing and making our community bigger and trustable.</h4>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 1</h4>
+                      </div>
+                      <h4 className="title">After minting the first point of action will be to establish the R&amp;D and fund wallets. This will form the foundation of our community</h4>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 2</h4>
+                      </div>
+                      <h4 className="title">Lunching more Advanced version of Creepto Universe NFT. </h4>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 3</h4>
+                      </div>
+                      <h4>Establishing governance for the wallets.initial funding for both wallets will be generated from royalies on secondary market as well as initially from minting.</h4>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 4</h4>
+                      </div>
+                      <h4 className="title">We will need to let the world know we exist and what we are we doing. we will use a host of methods and techniques to increase the reach and traction of the project.</h4>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 5</h4>
+                      </div>
+                      <h3 className="title">Establish our Standards. This will be primary utility for creepto universe project. This standards will give all potential investors in new NFT projects confidence that a reputable community has thorougly vetted and approved a project.</h3>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 6</h4>
+                      </div>
+                      <h4 className="title">To support us, we will establish a research division to be at the forefront of innovation.</h4>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Phase 7</h4>
+                      </div>
+                      <h3 className="title">Casual game production and competition</h3>
+                    </div>
+                  </div>
+                  <div className="timeline">
+                    <div className="timeline-content">
+                      <div className="timeline-date">
+                        <h4>Charities</h4>
+                      </div>
+                      <h3 className="title">10% of all our revenue raised through minting will be donated.</h3>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <h3 className="title">Journey to The Multiverse</h3>
-              <p>Minting begins. Space Explorars are freed on the Ethereum, Polygon Blockchain</p>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>2.</h4>
-              </div>
-              <h3 className="title"></h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Oct 1988</h4>
-              </div>
-              <h3 className="title">Formation of TOS Community Wallet</h3>
-              <p></p>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Oct 1988</h4>
-              </div>
-              <h3 className="title">Super Mario Bros. 3</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>April 1989</h4>
-              </div>
-              <h3 className="title">Super Mario Land</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Nov 1990</h4>
-              </div>
-              <h3 className="title">Super Mario World</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Oct 1992</h4>
-              </div>
-              <h3 className="title">Super Mario Land:<br/> 6 Golden Coins</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Aug 1995</h4>
-              </div>
-              <h3 className="title">Super Mario World 2:<br/> Yoshi's Island</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>June 1996</h4>
-              </div>
-              <h3 className="title">Super Mario 64</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>July 2002</h4>
-              </div>
-              <h3 className="title">Super Mario Sunshine</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>May 2006</h4>
-              </div>
-              <h3 className="title">New Super Mario Bros.</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Nov 2007</h4>
-              </div>
-              <h3 className="title">Super Mario Galaxy.</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Nov 2009</h4>
-              </div>
-              <h3 className="title">New Super Mario Bros. Wii</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>May 2010</h4>
-              </div>
-              <h3 className="title">Super Mario Galaxy 2</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Nov 2011</h4>
-              </div>
-              <h3 className="title">Super Mario 3D Land</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>July 2012</h4>
-              </div>
-              <h3 className="title">New Super Mario Bros 2</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Nov 2012</h4>
-              </div>
-              <h3 className="title">New Super Mario Bros. U</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Nov 2013</h4>
-              </div>
-              <h3 className="title">Super Mario 3D World</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Sept 2015</h4>
-              </div>
-              <h3 className="title">Super Mario Maker</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Dec 2016</h4>
-              </div>
-              <h3 className="title">Super Mario Run</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Oct 2017</h4>
-              </div>
-              <h3 className="title">Super Mario Odyssey</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>June 2019</h4>
-              </div>
-              <h3 className="title">Super Mario Maker 2</h3>
-            </div>
-          </div>
-          <div className="timeline">
-            <div className="timeline-content">
-              <div className="timeline-date">
-                <h4>Feb 2021</h4>
-              </div>
-              <h3 className="title">Super Mario 3D World<br/> + Bowser's Fury</h3>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
         </section>
     )
 }
