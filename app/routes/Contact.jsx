402682f3b881943ac3506f43c59cf15892764be9
creeptouniverse <creeptouniverse@gmail.com>
2022-02-24 14:58:08 +0545

    push

--- app/routes/Contact.jsx
+++ app/routes/Contact.jsx
@@ -1,7 +1,31 @@
 export default function Contact(){
     return(
-        <section>
+        <section className="gallery-section">
+            <h1>Gallery</h1>
+            <div className="gallery">
+         <div className="glass">
+             <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/56403242527071574379970211588888600945789997344969181986216295077285547999233">
+             <img className="gallery-image" src="https://lh3.googleusercontent.com/y-so5mpj6RXUhApyuY9x19wy4nZw7B6EN0mGuBcOHJ_fkO9tOKdxzT5CfvQV_lLDukvTtaG4Lvbbpz3p68iT4EJNJO1CYDzjJv7k"></img>
+</a>
+         </div>
+         <div className="glass">
+             <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/56403242527071574379970211588888600945789997344969181986216295075086524743681">
+         <img className="gallery-image" src="https://lh3.googleusercontent.com/TbBeq_I4Cpn-yff-Qc-94zOhQ2ztHl1ZORuoxx9SyW55U1ajrVZnzGSAmJ831ZM5O-GyMexL_SBGrjZG_gh9IvNBc2w6bfVHQheyxNQ"></img>
+</a>
+         </div>
+         <div className="glass">
+         <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/56403242527071574379970211588888600945789997344969181986216295059693361954826">
+         <img className="gallery-image" src="https://lh3.googleusercontent.com/gGK1KEJD989NR_XMu3ecWUJ-hc1EpDAvQ0zluWWMh0m7FDHUfhBaUQ7FPIuuw_UbA9VnsAf204-OKJvVvHU4Ium_2lpHSOhaaTnNxEY"></img>
+</a>
 
+         </div>
+         <div className="glass">
+             <a href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/564032425270715743799702115888886009457899973449691819862162950607928735826220">
+         <img className="gallery-image" src="https://lh3.googleusercontent.com/hA6HAEAtR2WwnyBCYUSmhE1bIcX_navRWjcVP0kQ3B6lcBQZgAoAybjovwnPqK8LYviGHV5LnWEWqBZtTMeFLEFbmFKwt3tnjnSu4w"></img></a>
+
+
+         </div>
+</div>
         </section>
     )
 }
--- app/routes/Roadmap.jsx
+++ app/routes/Roadmap.jsx
@@ -2,76 +2,94 @@ import { Link } from "remix";
 export default function Roadmap(){
     return(
       <section className="section-roadmap">
-        
-      {/* Vertical Timeline */}
-      <div id="conference-timeline">
-        <div className="timeline-start">Start</div>
-        <div className="conference-center-line" />
-        <div className="conference-timeline-content">
-          {/* Article */}
-          <div className="timeline-article">
-            <div className="content-left-container">
-              <div className="content-left">
-                <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. Mankind, let us preserve and increase this beauty, and not destroy it! <span className="article-number">01</span></p>
+        <p className="map-roadmap">ROADMAP
+        <time className="time" dateTime="2008-02-02">
+                      <span className="time__day">V - 0.01</span>
+                    </time>        </p>
+        <div>
+        <div className="page">
+          <div className="timeline">
+            <div className="timeline__group">
+              <span className="timeline__year time" aria-hidden="true">Phase - 1</span>
+              <div className="timeline__cards">
+                <div className="timeline__card card">
+                  <header className="card__header">
+                    <time className="time" dateTime="2008-02-02">
+                      <span className="time__day">Building Community</span>
+                      <span className="time__month"></span>
+                    </time>
+                  </header>
+                  <div className="card__content">
+                    <p>Right now we are at the initial phase of building our community trustable.</p>
+                  </div>
+                </div>
+                <div className="timeline__card card">
+                  <header className="card__header">
+                  <time className="time" dateTime="2008-02-02">
+                      <span className="time__day">Project Lunch</span>
+                      <span className="time__month">March 20</span>
+                    </time>
+                  </header>
+                  <div className="card__content">
+                  <p>We will start the official sale of Creepto Universe collection from March 20. A total of 200 unique NFT projects will be available to mint from March 20.</p>
+                  </div>
+                </div>
               </div>
-              <span className="timeline-author"></span>
             </div>
-            <div className="content-right-container">
-              <div className="content-right">
-                <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. <span className="article-number">02</span></p>
-              </div>
-              <span className="timeline-author"></span>
-            </div>
-            <div className="meta-date">
-              <span className="date">18</span>
-              <span className="month">APR</span>
-            </div>
-          </div>
-          {/* // Article */}
-          {/* Article */}
-          <div className="timeline-article">
-            <div className="content-left-container">
-              <div className="content-left">
-                <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. Mankind, let us preserve and increase this beauty, and not destroy it! <span className="article-number">01</span></p>
-              </div>
-              <span className="timeline-author"></span>
-            </div>
-            <div className="content-right-container">
-              <div className="content-right">
-                <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. <span className="article-number">02</span></p>
-              </div>
-              <span className="timeline-author"></span>
-            </div>
-            <div className="meta-date">
-              <span className="date">18</span>
-              <span className="month">APR</span>
-            </div>
-          </div>
-          {/* // Article */}
-          {/* Article */}
-          <div className="timeline-article">
-            <div className="content-left-container">
-              <div className="content-left">
-                <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. Mankind, let us preserve and increase this beauty, and not destroy it! <span className="article-number">01</span></p>
-              </div>
-              <span className="timeline-author"></span>
-            </div>
-            <div className="content-right-container">
-              <div className="content-right">
-                <p>When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. <span className="article-number">02</span></p>
-              </div>
-              <span className="timeline-author"></span>
-            </div>
-            <div className="meta-date">
-              <span className="date">18</span>
-              <span className="month">APR</span>
-            </div>
-          </div>
-          {/* // Article */}
-        </div>
-        <div className="timeline-end">End</div>
-      </div>
+            <div className="timeline__group">
+              <span className="timeline__year time" aria-hidden="true">SUPPORT</span>
+              <div className="timeline__cards">
+                <div className="timeline__card card">
+                  <header className="card__header">
+                
+                  </header>
+                  <div className="card__content">
+                    <p>Everyone needs help. We will onboard community support members to deliver mental health support to our holders.
 
+</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="timeline__group">
+              <span className="timeline__year time" aria-hidden="true">Gifts</span>
+              <div className="timeline__cards">
+                <div className="timeline__card card">
+                  <header className="card__header">
+                        
+                  </header>
+                  <div className="card__content">
+                    <p>We will provide a free blockchain domain (. NFT) for free to all NFT holders with free portfolio freelance.
+</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="timeline__group">
+              <span className="timeline__year time" aria-hidden="true">The Sky's the limit</span>
+              <div className="timeline__cards">
+                <div className="timeline__card card">
+                  <header className="card__header">
+                
+                  </header>
+                  <div className="card__content">
+                    <p>Realising new version of creepto roadmap on lunch date(March 20)
+
+</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+               <div className="timeline__group">
+              <span className="timeline__year time" aria-hidden="true">!END</span>
+              
+            </div>
+          </div>
+        </div>
+        <div className="linktr">
+          <a href="https://linktr.ee/creeptouniverse" target="_blank" className="linktr__goal r-link">Get more from us</a>
+        </div>
+      </div>
       </section>
     )
 }
